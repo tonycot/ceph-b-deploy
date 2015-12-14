b581445a4a0c252568f6cacdf43cd91712644ee3
tonycot <atarasov@mirantis.com>
2015-12-14 07:41:35 -0500

    Prefight is added

---
+++ bash/pre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Prefight for ceph-deploy
+
+release="hammer"
+distro="el7"
+
+sfile="${HOME}/ceph-b-deploy/templates/ceph.repo"
+dfile="/etc/yum.repos.d/ceph.repo"
+
+
+cp $sfile $dfile 
+sed -i "s/{ceph-release}\/{distro}/$release\/$distro/g" $dfile $dfile && yum install -y ceph-deploy
+
+
