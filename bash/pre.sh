47d716b7eb7e818756cea3e0c08eed8cfdb02b13
tonycot <atarasov@mirantis.com>
2015-12-14 06:42:56 -0500

    Small Fixes

--- bash/pre.sh
+++ bash/pre.sh
@@ -6,7 +6,7 @@
 
 function ipvar {
   for i in `seq 1 $1`
-    do 
+    do
       read  -p "Enter ip address of node ${i}: " sshk
       echo -e "Host node${i} \n  Hostname node${i} \n  User $usrn \n" >> $HOME/.ssh/config
       ssh-copy-id ${usrn}@${sshk}
@@ -22,15 +22,11 @@ distro="el7"
 sfile="${HOME}/ceph-b-deploy/templates/ceph.repo"
 dfile="/etc/yum.repos.d/ceph.repo"
 
-
-
-# Keygen 
+# Keygen
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rs
 read  -p "Enter the number of nodes: " nnumb
 ipvar $nnumb
-
-
 
 cp $sfile $dfile
 sed -i "s/{ceph-release}\/{distro}/$release\/$distro/g" $dfile $dfile && yum install -y ceph-deploy
