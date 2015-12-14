ebaa8c1fa5332655394cbf948545e2e5f1c1f081
tonycot <atarasov@mirantis.com>
2015-12-14 09:08:11 -0500

    Fixes

--- bash/pre.sh
+++ bash/pre.sh
@@ -8,8 +8,6 @@ distro="el7"
 sfile="${HOME}/ceph-b-deploy/templates/ceph.repo"
 dfile="/etc/yum.repos.d/ceph.repo"
 
-
-cp $sfile $dfile 
+cp $sfile $dfile
 sed -i "s/{ceph-release}\/{distro}/$release\/$distro/g" $dfile $dfile && yum install -y ceph-deploy
-
-
+mv $dfile /etc/yum.repos.d/ceph-deploy.repo
